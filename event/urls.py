d3a7777d322e7de37b94d4c0ec67631d727db82f
Sven Neumann <seven.newman@icloud.com>
2022-04-19 19:12:23 +0000

    add delete detail and create view

--- event/urls.py
+++ event/urls.py
@@ -1,7 +1,11 @@
 
 from django.urls import path, include
-from .views import index
+from .views import index, detail, create, createevent, deleteevent
 
 urlpatterns = [
     path('', index),
+    path('detail/<id>/', detail),
+    path('create/', create),
+    path('createevent/', createevent),
+    path('deleteevent/<id>/', deleteevent),
 ]
--- event/views.py
+++ event/views.py
@@ -1,11 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect
 from django.http import HttpResponse
 from .models import Event
+from datetime import datetime
 # Create your views here.
 
 
 def index(request):
     context = {'events': Event.objects.all()}
     return render(request, 'event/index.html', context=context)
-    
-    
+
+def detail(request, id):
+    context = {'event': get_object_or_404(Event, id=id)}
+    return render(request, 'event/detail.html', context=context)
+
+def create(request):
+    return render(request, 'event/create.html')
+
+def createevent(request):
+    new_event = Event(title = request.POST['title'])
+    new_event.subtitle = request.POST['subtitle']
+    new_event.content = request.POST['content']
+    new_event.date = datetime.now()
+    new_event.save()
+    return HttpResponseRedirect("/")
+
+def deleteevent(request, id):
+  event_to_delete = Event.objects.get(id=id)
+  event_to_delete.delete()
+  return HttpResponseRedirect('/')
